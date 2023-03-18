8e9306f459a6715d82fc0c96568f81410ac1fd25
nelaps <neldnelaps@gmail.com>
2023-03-18 21:43:59 +0700

    + new app

---
+++ OAuthYandexDemo/OAuthYandexDemo/AuthViewController.swift
@@ -0,0 +1,67 @@
+//
+//  AuthViewController.swift
+//  OAuthYandexDemo
+//
+//  Created by Natalia Pashkova on 18.03.2023.
+//
+
+import UIKit
+import Foundation
+import WebKit
+
+public protocol AuthViewControllerDelegate : class {
+    func handleTokenChanged (token: String)
+}
+
+final class AuthViewController : UIViewController{
+    weak var delegate : AuthViewControllerDelegate?
+    private let webView = WKWebView()
+    private let clientId = ""
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+        
+        guard let request = request else {return}
+        webView.load(request)
+        webView.navigationDelegate = self
+    }
+    
+    func setupViews() {
+        view.backgroundColor =  .white
+        title = "Мои фото"
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(webView)
+        NSLayoutConstraint.activate([
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
+    }
+    
+    private var request : URLRequest? {
+        guard var urlComponents = URLComponents(string: "http://oauth.yandex.ru/authorize") else {return nil}
+        urlComponents.queryItems = [
+        URLQueryItem(name: "response_type", value: "token"),
+        URLQueryItem(name: "client_id", value: "\(clientId)")]
+        
+        guard let url = urlComponents.url else {return nil}
+        return URLRequest(url: url)
+    }
+}
+
+extension AuthViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        if let url = navigationAction.request.url{//,
+          // url.scheme == scheme {
+            let targetString =  url.absoluteString.replacingOccurrences(of: "#", with: "?")
+            guard let components = URLComponents(string: targetString) else {return}
+            let token = components.queryItems?.first(where: {$0.name == "access_token"})?.value
+            if let token = token {
+                delegate?.handleTokenChanged(token: token)}
+            
+            dismiss(animated: true, completion: nil)
+            
+        }
+    }
+}
---
+++ OAuthYandexDemo/OAuthYandexDemo/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  OAuthYandexDemo
+//
+//  Created by Natalia Pashkova on 18.03.2023.
+//
+
+import UIKit
+import WebKit
+
+
+
+class ViewController: UIViewController {
+    private let tableView = UITableView()
+    private var isFirst = true
+    private var token: String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        if isFirst {
+            updateData()
+        }
+        isFirst = false
+    }
+    
+    private func setupViews() {
+        view.backgroundColor =  .white
+        title = "Мои фото"
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
+    }
+    
+    
+    private func updateData() {
+        guard !token.isEmpty else {
+            let requestTokenViewController = AuthViewController()
+            requestTokenViewController.delegate = self
+            present(requestTokenViewController, animated: false, completion: nil)
+            return
+        }
+    }
+
+}
+
+extension ViewController : AuthViewControllerDelegate {
+    func handleTokenChanged (token: String) {
+        self.token = token
+        print("New token: \(token)")
+        updateData()
+    }
+}
+
